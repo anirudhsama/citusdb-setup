71766abd881af55aec37b6515209f61f02d6a5dc
Aditi Bhatnagar <aditi.bhatnagar@metarain.com>
2018-07-05 11:59:24 +0530

    Set up vm with worker nodes

---
+++ citus-vm-setup/README.md
@@ -0,0 +1,24 @@
+# Vagrant Citus DB demo 
+(Patched from : https://github.com/hung135/vagrant_citusdb)
+- This vagrant script will build a VM with 5 docker instances of postgres 9.6 citusdb.
+# Steps:
+- Ensure you have vagrant installed. If not, follow (http://sourabhbajaj.com/mac-setup/Vagrant/README.html)
+- clone this repo
+- cd into the directory 
+- execute: vagrant up
+- when up,  vagrant ssh 
+- inside vargrant vm, execute : sudo su ubuntu
+- run: docker ps 
+- should see all instances of postgres
+- run: psql -h localhost -U postgres -c "SELECT * FROM master_get_active_worker_nodes();"
+- node_name | node_port 
+- -----------+-----------
+-  localhost |      5434
+-  localhost |      5436
+-  localhost |      5433
+-  localhost |      5435
+- (4 rows)
+
+# Postgres
+- The master is port forwarded to 5432. 
+- Workers are port fowarded to 5433,5434,5435,5436
---
+++ citus-vm-setup/install_rdbms.sh
@@ -0,0 +1,22 @@
+#this will be last and most of the time we won't need and requires 4gb of ram to run
+docker build -t postgrescitus .
+      #docker run  --restart=always --name postgres -e POSTGRES_PASSWORD=ubuntu -p 54322:5432 -d postgres:latest   
+      # Add Citus repository for package manager
+      #https://docs.citusdata.com/en/v6.1/installation/production_deb.html
+      curl https://install.citusdata.com/community/deb.sh | sudo bash
+      # install locally sowe can have client tools 
+      sudo apt-get -y install postgresql-client-9.6 libpq5 pgdg-keyring postgresql-client-common
+
+      # preload citus extension
+      #sudo pg_conftool 9.6 main set shared_preload_libraries citus
+      docker run -d -p 5432:5432 --name master postgrescitus
+      docker run -d -p 5433:5432 postgrescitus
+      docker run -d -p 5434:5432 postgrescitus
+      docker run -d -p 5435:5432 postgrescitus
+      docker run -d -p 5436:5432 postgrescitus
+      # because Master is port forward to 192.168.33.11 5432 from above
+     psql -h localhost -U postgres -c "SELECT * from master_add_node('localhost', 5433);"
+     psql -h localhost -U postgres -c "SELECT * from master_add_node('localhost', 5434);"
+     psql -h localhost -U postgres -c "SELECT * from master_add_node('localhost', 5435);"
+     psql -h localhost -U postgres -c "SELECT * from master_add_node('localhost', 5436);"
+
